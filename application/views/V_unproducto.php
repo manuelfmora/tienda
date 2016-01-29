bc25e0640b5acad55973dc67bd71f3cea899aae5
Manuel Mora <Manuel Mora@PC-CUARTILLO>
2016-01-29 16:56:24 +0100

    Creamos Vista producto

--- application/views/V_unproducto.php
+++ application/views/V_unproducto.php
@@ -3,9 +3,9 @@
 <?php foreach($productos as $producto ): ?>
     <li><a href="#"><img class="img-responsive" width="200" src="<?=base_url().'assets/img/'.$producto->imagen?>"></a></li>
     <h4><?=$producto->nombre ?></h4></br>
-<h4><?=$producto->nombre ?></h4>
-<h4><?=$producto->nombre ?></h4>   
-<p><?=$producto->descripcion ?></p>   
+    <h4><?=$producto->nombre ?></h4>
+    <h4><?=$producto->nombre ?></h4>   
+    <p><?=$producto->descripcion ?></p>   
 <?php endforeach; ?>
 </ul>
 </div>
